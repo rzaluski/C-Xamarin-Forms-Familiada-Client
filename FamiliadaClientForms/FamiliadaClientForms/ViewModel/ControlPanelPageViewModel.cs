c6f89ca6616a133eb47e006224b9fd57a9bddf11
Rafał Załuski <rafal.zaluski@student.put.poznan.pl>
2018-12-04 22:54:13 +0100

    no message

--- FamiliadaClientForms/FamiliadaClientForms/ViewModel/ControlPanelPageViewModel.cs
+++ FamiliadaClientForms/FamiliadaClientForms/ViewModel/ControlPanelPageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Server;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
@@ -12,6 +14,8 @@ namespace FamiliadaClientForms.ViewModel
     {
         private TcpClient _tcpClient;
         private Page _page;
+        private Question _currentQuestion { get; set; }
+
         public ControlPanelPageViewModel(Page page, TcpClient tcpClient)
         {
             _page = page;
@@ -19,15 +23,41 @@ namespace FamiliadaClientForms.ViewModel
             RandQuestion = new Command(OnRandQuestion);
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Question CurrentQuestion
+        {
+            get { return _currentQuestion; }
+            set
+            {
+                _currentQuestion = value;
+                OnPropertyChanged("CurrentQuestion");
+            }
+        }
+
         public ICommand RandQuestion { get; set; }
         void OnRandQuestion()
         {
             Stream stream = _tcpClient.GetStream();
             ASCIIEncoding asen = new ASCIIEncoding();
-            TableCommand tableCommand = new TableCommand("RandQuestion");
-            string msgString = JMessage.CreateMessage("TableCommand", tableCommand);
+            string msgString = JMessage.CreateMessage("RandQuestion", null);
             byte[] b = asen.GetBytes(msgString);
             stream.Write(b, 0, b.Length);
+
+            byte[] bb = new byte[1000];
+            int k = stream.Read(bb, 0, 1000);
+            msgString = System.Text.Encoding.ASCII.GetString(b, 0, k);
+            JMessage msg = JMessage.Deserialize(msgString);
+            CurrentQuestion = JMessage.Deserialize<Question>(msg.ObjectJson);
         }
     }
 }
